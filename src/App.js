8df1769b8f467fa0e1b98630654ed092621ea444
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-07-31 00:21:33 +0300

    General Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import { Container } from "react-bootstrap";
+import HeroPart from "./components/HeroPart";
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+import Companies from "./components/Companies";
+import CallToAction from "./components/CallToAction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      {/* <Container className="App"> */}
+        <HeroPart />
+        <Highlights/>
+        <CallToAction/>
+      {/* </Container> */}
+      <Companies/>
     </div>
   );
 }
